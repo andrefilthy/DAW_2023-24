62ee4f0b5257183fae6ca332d31ed78e0632fb1e
jorgepreis <jorgeheaton4@gmail.com>
2023-12-08 12:22:27 +0000

    front end initiation

--- gomoku/js/src/pages/Home.tsx
+++ gomoku/js/src/pages/Home.tsx
@@ -1,88 +1,25 @@
-import * as React from 'react'
-import {useState, useEffect} from 'react'
-import {useNavigate} from 'react-router-dom'
-import { TopBar } from '../components/TopBar'
-import { fetchHome } from '../ApiCalls'
-import { Authenticate } from '../components/Authenticate'
-import { startGame, getGameByUser } from '../ApiCalls'
-import { pollGame } from './Game'
-import useCookies from 'react-cookie/cjs/useCookies'
-import { useAuthCookie } from '../authContainer'
-import { withCookies, Cookies } from 'react-cookie';
+import * as React from "react"
 
-
-export type LinkRelation = {
-    rel : string[],
-    href : string
+type Link = {
+    name:string,
+    path:string
 }
 
-export type User = {
-    username : string,
-    password : string
-}
+const links :Link[] = 
+[
+    {name:"register", path:"./register"},
+    {name:"login", path: "./login"},
+    //Se user estiver logado, então aparecem links diferentes.
+]
 
 
-type HomeProps = {
-    user : User | null,
-    self : LinkRelation
-    links : LinkRelation[]
-    action : null
-}
 
-export function Home(): React.ReactElement {
-    const [props, setProps] = useState<HomeProps | null>(null)
-    const cookies = useAuthCookie()
-    const navigate = useNavigate()
-
-    const token = cookies["access_token"]
-
-    console.log(token)
-    let contents = token === undefined ? 
-        <Authenticate /> :
-        <button onClick={() => searchGame()}>Play</button>
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const data = await fetchHome(token)
-            const sirenobj = await data.json()
-            var arr = sirenobj.links as LinkRelation[]
-            const self = arr.find(e => e.rel.includes("self"))
-
-            setProps({
-                user : sirenobj.user,
-                self : arr.splice(arr.indexOf(self))[0],
-                links : arr,
-                action : null
-            })
-          }
-
-        fetchData()
-    },[token])
-
-    return (
+export default function Home():React.ReactElement
+{
+    return(
         <div>
-            {props && <div>
-                <h1>Home</h1>
-                {<TopBar home={props.self} links={props.links} />}
-                {contents}
-            </div>}
+            <h1>Olá</h1>
         </div>
     )
-
-    async function searchGame(){
-        const info = await startGame(token)
-        if(info.status == 201){
-            const data = await info.json()
-            navigate(`/game/${data.properties.gameID}`, { state : data})
-            return
-        }else if(info.status == 200){
-            pollGame(token).then(game => {
-                navigate(`/game/${game.properties.gameID}`, { state : game})
-            })
-        }else if(info.status == 403){
-            const data = await info.json()
-            alert(data.message)
-        }
-        
-    }
 }
+
--- gomoku/js/webpack.config.js
+++ gomoku/js/webpack.config.js
@@ -1,36 +1,52 @@
-module.exports = {
-    mode: "development",
-    resolve: {
-        extensions: [".js", ".ts", ".tsx"]
-    },
-    devServer: {
-        historyApiFallback: true,
-        port: 9000,
-        proxy: {
-            '/api': {
-                target: {
-                   host: "0.0.0.0",
-                   protocol: 'http:',
-                   port: 8080
-                },
-                pathRewrite: {
-                   '^/api': ''
-                },
-            },        
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader","css-loader"],
-            }
-        ]  
-    }
+const ESLintPlugin = require('eslint-webpack-plugin');
+function delay(ms) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, ms);
+  });
 }
-    
+module.exports = {
+  mode: 'development',
+  devServer: {
+    port: 8000,
+    historyApiFallback: true,
+    compress: false,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        // introducing an API delay to make testing easier
+        pathRewrite: async function (path, req) {
+          await delay(1000);
+          return path;
+        },
+        onProxyRes: (proxyRes, req, res) => {
+          proxyRes.on('close', () => {
+            if (!res.writableEnded) {
+              res.destroy();
+            }
+          });
+          res.on('close', () => {
+            console.log("req close");
+            proxyRes.destroy();
+          });
+        },
+      },
+    },
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+  plugins: [
+    new ESLintPlugin({
+      extensions: ['js', 'jsx', 'ts', 'tsx'],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
