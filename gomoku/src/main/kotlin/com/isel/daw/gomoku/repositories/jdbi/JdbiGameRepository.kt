0ddbdd7bfc916bfd0dbcd2309672e8dccf76c0fe
44832 Hugo martins <A44832@alunos.isel.pt>
2023-12-08 12:14:03 +0000

    Add logic for waiting list

--- gomoku/src/main/kotlin/com/isel/daw/gomoku/repositories/jdbi/JdbiGameRepository.kt
+++ gomoku/src/main/kotlin/com/isel/daw/gomoku/repositories/jdbi/JdbiGameRepository.kt
@@ -2,15 +2,26 @@ package com.isel.daw.gomoku.repositories.jdbi
 
 import com.isel.daw.gomoku.domain.*
 import com.isel.daw.gomoku.repositories.GamesRepository
+import isel.daw.proj.dtos.WaitingEntry
 import org.jdbi.v3.core.Handle
 import org.jdbi.v3.core.mapper.Nested
 import java.time.Instant
 import java.util.*
 
 class JdbiGameRepository(private val handle : Handle) : GamesRepository {
-    override fun getByUser(user: User): Game? {
-        TODO("Not yet implemented")
-    }
+    override fun getByUser(user: User): Game? =
+        handle.createQuery(
+            "SELECT games.gameID, games.ruleSet, games.created, games.currentPhase, games.currentState, games.board1, games.board2, games.player1_logic, games.player2_logic, games.turnStartedAt, " +
+                    "player1.username as player1_username, player1.pwd as player1_pwd, player1.numberOfGames as player1_numberOfGames, player1.numberOfWins as player1_numberOfWins, " +
+                    "player2.username as player2_username, player2.pwd as player2_pwd, player2.numberOfGames as player2_numberOfGames, player2.numberOfWins as player2_numberOfWins" +
+                    " FROM dbo.game games " +
+                    "inner join dbo.User player1 on games.player1 = player1.username " +
+                    "inner join dbo.User player2 on games.player2 = player2.username " +
+                    "WHERE (player1.username = :username or player2.username = :username) and games.currentphase != 'COMPLETED'")
+            .bind("username", user.username)
+            .mapTo(GameDbModel::class.java)
+            .singleOrNull()
+            ?.run { toGame() }
 
     override fun getById(id: UUID): Game? =
         handle.createQuery(
@@ -57,6 +68,27 @@ class JdbiGameRepository(private val handle : Handle) : GamesRepository {
             .execute()
     }
 
+    override fun insertToWaitingList(entry: WaitingEntry) {
+        handle.createUpdate("insert into dbo.waitinglist (player, ruleset) values (:player, :ruleset)")
+            .bind("player", entry.player1.username)
+            .bind("ruleset", entry.ruleSet.toString())
+            .execute()
+    }
+
+    override fun searchForWaitingEntry(ruleSet: RuleSet): WaitingEntry? =
+        handle.createQuery("select w.ruleset, player1.username as player1_username, player1.pwd as player1_pwd, player1.numberOfGames as player1_numberOfGames, player1.numberOfWins as player1_numberOfWins from dbo.waitinglist w "
+                + "inner join dbo.user player1 on w.player = player1.username where w.ruleset = :ruleset")
+            .bind("ruleset", ruleSet.toString())
+            .mapTo(WaitingEntryDbModel::class.java)
+            .singleOrNull()
+            ?.run { toWaitingEntry() }
+
+    override fun deleteEntryFromWaitingList(player: User) {
+        handle.createUpdate("delete from dbo.waitinglist where player = :player")
+            .bind("player", player.username)
+            .execute()
+    }
+
 }
 class GameDbModel(
     val gameID: UUID,
@@ -85,5 +117,16 @@ class GameDbModel(
         player1Logic = player1Logic,
         player2Logic = player2Logic,
         turnStartedAt = turnStartedAt
+    )
+}
+
+class WaitingEntryDbModel(
+    @Nested("player1")
+    val player1 : User,
+    val ruleSet: RuleSet
+){
+    fun toWaitingEntry() = WaitingEntry(
+        player1 = player1,
+        ruleSet = ruleSet
     )
 }
