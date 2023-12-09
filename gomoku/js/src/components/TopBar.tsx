ea695ab85380d934a1eac44937aa42f385f00073
jorgepreis <jorgeheaton4@gmail.com>
2023-12-09 03:32:56 +0000

    need to work on context

--- gomoku/js/src/components/TopBar.tsx
+++ gomoku/js/src/components/TopBar.tsx
@@ -1,6 +1,5 @@
 import * as React from "react"
 import { useNavigate } from "react-router-dom"
-import { useEffect } from "react"
 
 export type Link = {
     name:string,
@@ -12,11 +11,6 @@ type Props = {
 
 export default function TopBar ({links}:Props) : React.ReactElement {
     const navigate = useNavigate()
-    useEffect(() => {
-        console.log("Chamado")
-        console.log(links)
-        console.log(localStorage.getItem("accessToken"))
-    },[links])
 
     if(localStorage.getItem("accessToken") != null) {
         links.splice(links.indexOf({name:"Logout", path:"/"}, 1)) 
--- gomoku/js/src/pages/Home.tsx
+++ gomoku/js/src/pages/Home.tsx
@@ -1,24 +1,18 @@
 import * as React from "react"
 import TopBar, {Link} from '../components/TopBar'
-import { useState, useEffect } from "react"
 
 export default function Home():React.ReactElement
 {
-    const [links, setLinks] = useState<Link[]>([])
-    useEffect(() => {
-        console.log("HOME")
-        if(localStorage.getItem("accessToken") == null){
-            setLinks([
-                {name:"Register", path:"/register"},
-                {name:"Login", path: "/login"},
+    const links : Link[] = (localStorage.getItem("accessToken") == null)?
+        ([
+            {name:"Register", path:"/register"},
+            {name:"Login", path: "/login"},
             ])
-        } else {
-            setLinks([
+        :
+            ([
                 {name:"Play", path:"/game"},
                 {name:"Logout", path:"/"}
             ] )
-        }
-    }, [localStorage.getItem("accessToken")])
 
     return(
         <div>
